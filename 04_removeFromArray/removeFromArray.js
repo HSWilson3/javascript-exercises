72e799ef5f50fddb9ead140b07c7514e2693a8d9
HSWilson3 <101376727+HSWilson3@users.noreply.github.com>
2025-02-13 19:59:44 -0600

    Complete 4th exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,29 @@
-const removeFromArray = function() {
+const removeFromArray = function(inputArray, ...items) {
+//function removeFromArray(inputArray, ...items) { // delete for testing
+    //console.log("input array is " + inputArray);
+    //console.log("items to remove are " + items);
+    let outputArray = [];
+    for (let i = 0; i < inputArray.length; i++) {
+        //console.log("checking " + inputArray[i]);
+        let flag = 0;
+        for (const item of items) {
+            //console.log("against " + item);
+            if (inputArray[i] === item) {
+                //console.log("they're the same")
+                flag = 1;
+            } //else {
+                //console.log("not the same, adding " + inputArray[i] + " to output")
+            //}
+        }
+        if (flag === 0) {
+            outputArray.push(inputArray[i]);
+        }
+        
+    }
+//    console.log("output array is " + outputArray);
+    return outputArray;
 };
 
+//removeFromArray([1, 2, 3, 4], 3, 2); // delete for testing
 // Do not edit below this line
-module.exports = removeFromArray;
+module.exports = removeFromArray; // uncomment for testing
