77b24ebe7e34ddbc10fefc4e969f813e67485dfb
HSWilson3 <101376727+HSWilson3@users.noreply.github.com>
2025-02-13 20:20:05 -0600

    Complete exercise sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,31 @@
-const sumAll = function() {
+const sumAll = function(first, last) {
+    if (!(typeof first === "number") || 
+        !(typeof last === "number") || 
+        !(Math.floor(first) === first) || 
+        !(Math.floor(last) === last) ||
+        (first < 0) ||
+        (last < 0)) {
 
+        return "ERROR";
+
+    } else {
+
+        if (first > last) {
+            let temp;
+            temp = first;
+            first = last;
+            last = temp;
+        }
+
+        let sum = 0;
+
+        for (let i = first; i <= last; i++) {
+            sum = sum + i;
+        }
+        
+        return sum;
+    }
+    
 };
 
 // Do not edit below this line
