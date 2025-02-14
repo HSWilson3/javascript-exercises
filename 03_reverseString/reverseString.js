d416b7630b146aee4d61dbc2c1e34289b13e2e33
HSWilson3 <101376727+HSWilson3@users.noreply.github.com>
2025-02-13 19:14:17 -0600

    Complete first three exercises

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,18 @@
-const reverseString = function() {
-
+const reverseString = function(input) {
+    let inputArray = input.split("");
+    let outputArray = [];
+    for (let i = 0; i < input.length; i++) {
+        let letter = inputArray.pop();
+        outputArray.push(letter);
+    }
+    let outputString = outputArray.toString();
+    let final = "";
+    for (let i = 0; i < outputString.length; i++) {
+        if (i % 2 === 0 || i === 0) {
+            final = final + outputString[i]
+        }
+    }
+    return final;
 };
 
 // Do not edit below this line
