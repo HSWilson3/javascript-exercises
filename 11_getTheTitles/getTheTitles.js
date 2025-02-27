5f93da14db9f22488c1bb50058c397746a488451
HSWilson3 <101376727+HSWilson3@users.noreply.github.com>
2025-02-26 19:11:18 -0600

    Complete exercise 11_getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,8 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arr) {
+    // iterate through each item of the array
+    // each item is an object
+    // map item to object.title
+    return arr.map((item) => item.title);
 };
 
 // Do not edit below this line
