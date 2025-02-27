c4d8dd539e4747e2f5447104cc299a8741fab4f7
HSWilson3 <101376727+HSWilson3@users.noreply.github.com>
2025-02-26 18:40:49 -0600

    Complete exercise 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,31 @@
-const palindromes = function () {
+const palindromes = function (str) {
+// convert string to all lowercase
+// convert string to array
+// iterate over array
+// filter only alpha characters
+// create new empty array
+// add from old to new, reversing order (shift)
+    const lowerString = str.toLowerCase();
+    const origArray = [];
+    let filteredString = "";
+    for (let i = 0; i < lowerString.length; i++) {
+        if ((lowerString.charCodeAt(i) >= 97 && lowerString.charCodeAt(i) <= 122) || (lowerString.charCodeAt(i) >= 48 && lowerString.charCodeAt(i) <= 57)) {
+            origArray.push(lowerString[i]);
+            filteredString += lowerString[i];
+        }
+    }
 
+    const reversedArray = [];
+    origArray.forEach((char) => {
+        reversedArray.unshift(char);
+    });
+    const reversedString = reversedArray.join("");
+
+    if (filteredString === reversedString) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
