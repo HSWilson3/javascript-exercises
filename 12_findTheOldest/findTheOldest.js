e9ad1a28a8211656884793d554aa996229f2334e
HSWilson3 <101376727+HSWilson3@users.noreply.github.com>
2025-02-26 19:47:22 -0600

    Complete exercise 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    // for each item in array
+    // check whether there is a yearOfDeath
+    // if not, find today's date and subtract yearOfBirth
+    // map item.age to the result
+    // if there is yearOfDeath, find yearOfDeath - yearOfBirth
+    // map item.age to the result
 
+    const currentYear = new Date().getFullYear();
+    arr.forEach((item) => {
+        if (!item.yearOfDeath) {
+            console.log("not dead");
+            item.age = currentYear - item.yearOfBirth;
+        } else {
+            console.log("dead");
+            item.age = item.yearOfDeath - item.yearOfBirth;
+        }
+    })
+    const sorted = arr.sort((a, b) => b.age - a.age);
+    return sorted[0];
 };
 
 // Do not edit below this line
