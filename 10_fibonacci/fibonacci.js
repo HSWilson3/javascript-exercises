ffb7e2f990a252f2d5b3b679fa64940bfe4dcdd0
HSWilson3 <101376727+HSWilson3@users.noreply.github.com>
2025-02-26 19:07:25 -0600

    Complete exercise 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    const toNum = Number(num);
+    if (toNum < 0) {
+        return "OOPS";
+    } else if (toNum === 0) {
+        return 0;
+    }
+    let sequence = [1, 1];
+    for (let i = 2; i <= toNum; i++) {
+        sequence[i] = sequence[i-1] + sequence[i-2];
+    }
+    return sequence.at(toNum - 1);
 };
 
 // Do not edit below this line
